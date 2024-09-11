6e1482679d7ae425ce84c649a77e7b2959fc5b3f
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-09-11 13:01:34 -0500

    🐛 URL definition

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -7,7 +7,7 @@ import { lastValueFrom } from 'rxjs'
 
 @Injectable()
 export class PostsService {
-  private readonly graphqlEndpoint = 'persistence-layer/graphql:3000'
+  private readonly graphqlEndpoint = 'persistence-layer:3000/graphql'
 
   constructor(private readonly httpService: HttpService) {}
 
